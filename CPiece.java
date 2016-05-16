a3bc3199d7102f86a5d3f6a64730c5af3bd99678
Harry <lilharry221@gmail.com>
2016-05-16 00:12:32 -0400

    add possible data types

---
+++ CPiece.java
@@ -0,0 +1,10 @@
+public class CenterPiece extends Piece{
+    private String color;
+    public Piece(String color){
+	this.color = color;
+    }
+
+    public String getColor(){
+	return color;
+    }
+}
